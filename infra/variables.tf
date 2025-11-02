c008c0cfbd4f7aabb30e225c45dcccfd0557117b
alkateca <chesini.mendes@gmail.com>
2025-11-02 14:45:17 -0300

    teste de pipeline

---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  description = "Nome do Resource Group"
+  type = string
+  default = "rg-vm-automation"
+}
+
+variable "location" {
+  description = "Região do Azure"
+  type = string
+  default = "East US 2"
+}
+
+variable "admin_password" {
+  description = "Senha do usuário adm"
+  type = string
+  sensitive = true
+  default = "Senhaforte123!@#"
+}
